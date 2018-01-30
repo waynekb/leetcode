be10e69c15bf1f38066e7a4ca19df22416c2729d
胡伟 <342818240@qq.com>
2018-01-30 16:52:56 +0800

    1-30 几种排序算法实现

--- C++/EASY/sort.cc
+++ C++/EASY/sort.cc
@@ -1,10 +1,11 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 using std::vector;
 using std::cout;
 using std::cin;
 using std::endl;
-
+using std::swap;
 /**
  * 冒泡排序
 */
@@ -149,71 +150,195 @@ using std::endl;
 /**
  * 希尔排序
 */
+// class Solution
+// {
+// public:
+//     void sort(vector<int> &nums)
+//     {
+//         cout << " 希尔排序 " << endl;
+//         int size = nums.size();
+//         int interval = size;
+//         int mult = 2;
+//         while (1)
+//         { //loop times
+//             interval = size / mult;
+//             for (int i = 0; i < interval; i++)
+//             {
+//                 for (int j = i + interval; j < size; j += interval)
+//                 {
+//                     for (int k = j; k >= interval; k -= interval)
+//                     {
+//                         if (nums[k - interval] > nums[k])
+//                         {
+//                             int temp = nums[k];
+//                             nums[k] = nums[k - interval];
+//                             nums[k - interval] = temp;
+//                         }
+//                         else{
+//                             break;
+//                         }
+//                     }
+//                 }
+//             }
+//             mult *= 2;
+//             if (interval == 1)
+//                 return;
+//         }
+//     }
+// };
+
+/**
+ * 希尔排序
+*/
+
+// class Solution
+// {
+//     public:
+//     void sort(vector<int> &nums)
+//     {
+//         for (int gap = nums.size() >> 1; gap > 0; gap >>= 1)
+//         { // times
+//             for (int i = gap; i < nums.size(); i++)
+//             { // position
+//                 int temp = nums[i];
+
+//                 int j = i - gap;
+//                 for (; j >= 0 && nums[j] > temp; j -= gap)
+//                 {
+//                     nums[j + gap] = nums[j];
+//                 }
+
+//                 nums[j + gap] = temp;
+//             }
+//         }
+//     }
+// };
+
+
+/**
+ * 快速排序
+ * 基准数字的位置与先从右边开始找还从左边开始找是有联系的。
+ * 若基准数字在左边，则应该先从右边开始找，这样找到的最后位置肯定是
+ * 数值比基准数字小。
+ * 若从左边开始找，则找到的最后位置肯定是数值比基准数字大。
+*/
+// class Solution
+// {
+//   public:
+//     void sort(vector<int> &nums)
+//     {
+            // cout << " 快速排序 " << endl;
+//         quick_sort(nums,0,nums.size()-1);
+//     }
+//   private:
+//     void quick_sort(vector<int> &nums, int cl, int cr)
+//     {
+//         int l = cl;
+//         int r = cr;
+//         int mark = l;
+//         if (l < r)
+//         {
+//             while (l < r)
+//             {
+//                 while (nums[r] >= nums[mark] && l < r)
+//                     r--;
+//                 while (nums[l] <= nums[mark] && l < r)
+//                     l++;
+//                 swap(nums[l], nums[r]);
+//             }
+//             swap(nums[r], nums[mark]);
+//             quick_sort(nums, cl, l);
+//             quick_sort(nums, l + 1, cr);
+//         }
+//     }
+// };
+
+
+
+
+/**
+ * 堆排序
+*/
+// class Solution
+// {
+//   public:
+//     void sort(vector<int> &nums)
+//     {
+//         cout << " 堆排序 " << endl;
+//         int n=nums.size();
+//         for(int i = n/2-1; i>=0 ;i--)//n是以数组中的数字第几位计算
+//         {
+//             max_heapify(nums, i, n-1);
+//         }
+//         for(int i=n-1; i>0;i--){
+//             swap(nums[0],nums[i]);
+//             max_heapify(nums,0,i-1);
+//         }
+//     }
+
+//   private:
+//     void max_heapify(vector<int> &nums, int beg, int end)//beg和end是以数组下标计算
+//     {
+//         int parent=beg;
+//         int child=parent*2+1;
+//         while(child<=end){
+//             if(child+1<=end && nums[child+1] > nums[child])
+//                 child++;
+//             if(nums[child] > nums[parent]){
+//                 swap(nums[child],nums[parent]);
+//                 parent=child;
+//                 child=parent*2+1;
+//             }
+//             else break;
+//         }
+//     }
+// };
+
+
+/**
+ * 归并排序
+*/
 class Solution
 {
   public:
     void sort(vector<int> &nums)
     {
-        cout << " 希尔排序 " << endl;
-        int size = nums.size();
-        int interval = size;
-        int mult = 2;
-        while (1)
-        { //loop times
-            interval = size / mult;
-            for (int i = 0; i < interval; i++)
-            {
-                for (int j = i + interval; j < size; j += interval)
-                {
-                    for (int k = j; k > 0; k -= interval)
-                    {
-                        if (nums[k - interval] > nums[k])
-                        {
-                            int temp = nums[k];
-                            nums[k] = nums[k - interval];
-                            nums[k - interval] = temp;
-                        }
-                    }
-                }
+        cout << " 归并排序 " << endl;
+        int size=nums.size();
+        int i=1;
+        do{
+            i*=2;
+            int beg=0;
+            int end=i-1>size-1?size-1:i-1;
+            while(beg<=size){
+                merge_sort(nums,beg,end);
+                beg+=i;
+                end=end+i>size-1?size-1:end+i;
             }
-            mult *= 2;
-            if (interval == 1)
-                return;
+        }while(i < size);
+    }
+    private:
+    void merge_sort(vector<int> &nums,int beg,int end){
+        for(int i=beg+1;i<=end;i++){
+            for(int j=i;j>beg;j--){
+                if(nums[j-1]>nums[j]){
+                    swap(nums[j],nums[j-1]);
+                }
+                else break;
+            }
         }
     }
 };
 
-    // class Solution
-    // {
-    //   public:
-    //     void sort(vector<int> &nums)
-    //     {
-    //         for (int gap = nums.size() >> 1; gap > 0; gap >>= 1)
-    //         { // times
-    //             for (int i = gap; i < nums.size(); i++)
-    //             { // position
-    //                 int temp = nums[i];
-
-    //                 int j = i - gap;
-    //                 for (; j >= 0 && nums[j] > temp; j -= gap)
-    //                 {
-    //                     nums[j + gap] = nums[j];
-    //                 }
-
-    //                 nums[j + gap] = temp;
-    //             }
-    //         }
-    //     }
-    // };
-
-    int main()
+int main()
+{
+    Solution sol;
+    // vector<int> a {25, 34, -4, 0, 4, 9, 6, -1, 7, 2, 7, 25, 19, 20,-23,-11,43,29,1,0,0};
+    vector<int> a {45, 34, 0, -4, 8,5,8};
+    sol.sort(a);
+    for (auto &w : a)
     {
-        Solution sol;
-        vector<int> a = {-1, 25, 34, -4, 0, 4, 9, 6, -1, 7, 2, 7, 25, 19, 20};
-        sol.sort(a);
-        for (auto &w : a)
-        {
-            cout << w << " ";
-        }
-        cout << endl;
+        cout << w << " ";
     }
+    cout << endl;
+}
